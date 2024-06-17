0fe0e9f1c52429b8caaa52300b59a3d0ee11c46e
ricardo <ridaoc19@gmail.com>
2024-06-17 23:55:31 +0200

    feat(button): add global button

---
+++ src/components/common/button/Button.tsx
@@ -0,0 +1,68 @@
+import React, { ButtonHTMLAttributes, ReactNode } from 'react';
+
+import Svg from '../icons/Svg';
+import { SvgType } from '../icons/svgType';
+import { ButtonType } from './button.type';
+import _color from '../../../styles/main/global/_color';
+import { HandleClick } from '../../../interfaces/global';
+
+export interface ButtonProps {
+	id: string;
+	type: ButtonType;
+	text: string | ReactNode;
+	handleClick: HandleClick;
+
+	svgRight?: SvgType;
+	svgLeft?: SvgType;
+	other_attributes?: ButtonHTMLAttributes<HTMLButtonElement>;
+	disabled?: boolean;
+	value?: string | number;
+	className?: string;
+}
+
+const Button: React.FC<ButtonProps> = (params: ButtonProps) => {
+	const { id, type, handleClick, text, value, disabled, className, other_attributes, svgLeft, svgRight } = params;
+	return (
+		<button
+			id={id}
+			className={`button button_${type} ${className}`}
+			onClick={handleClick}
+			value={value}
+			disabled={disabled}
+			aria-label={`Click para ${id}`}
+			{...other_attributes}
+		>
+			{!!svgLeft && (
+				<span className='button__svg-left'>
+					{Svg({
+						type: svgLeft,
+						height: 16,
+						width: 16,
+						color: [ButtonType.Error, ButtonType.Success, ButtonType.Warning, ButtonType.Information].includes(type)
+							? _color['--font-light']
+							: undefined,
+					})}
+				</span>
+			)}
+			{!!text && (
+				<span className='button__text-container'>
+					<div>{text}</div>
+				</span>
+			)}
+			{!!svgRight && (
+				<span className='button__svg-right'>
+					{Svg({
+						type: svgRight,
+						height: 16,
+						width: 16,
+						color: [ButtonType.Error, ButtonType.Success, ButtonType.Warning, ButtonType.Information].includes(type)
+							? _color['--font-light']
+							: undefined,
+					})}
+				</span>
+			)}
+		</button>
+	);
+};
+
+export default Button;
